bb4b3e60b199c3a089f004fea1aa37abf0870908
vivekchandran <vivekchandran663@gmail.com>
2024-08-10 22:59:53 +0530

    initial commit

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -4,61 +4,61 @@ import BookCard from "../components/BookCard";
 function Home(props) {
     const [books] = useState([
         {
-            id: 1,
+            Id: 1,
             title: "Bennyaminte Aadhujeevitham",
             author: "Bennyamin",
             image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8UKn-KAIHv6r8-FuH_gNpAxP56kPMO8_VGw&s"
         },
         {
-            id: 2,
+            Id: 2,
             title: "Aarachaar",
             author: "K R Meera",
             image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdGa2E3SToT3YYWWjkBBdZClVEBmaUJcogiQ&s"
         },
         {
-            id: 3,
+            Id: 3,
             title: "Pathummayude Aadu",
             author: "Basheer",
             image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpdcNvSbSsaaxEKBZR6-P7jV2vVIyp6WIU_jjz6db7yoB-EV6UhXX7o3I417z0ty7qEJw&usqp=CAU"
         },
         {
-            id: 4,
+            Id: 4,
             title: "Indhulekha",
             author: "O Chandhumenon",
             image: "https://cdn.exoticindia.com/images/products/original/books-2019-022/uau900.webp"
         },
         {
-            id: 5,
+            Id: 5,
             title: "Ram C/O Aanandhi",
             author: "Akhil P Dharmajan",
             image: "https://m.media-amazon.com/images/I/61De10B-ooL._AC_UF1000,1000_QL80_.jpg"
         },
         {
-            id: 6,
+            Id: 6,
             title: "Kaalam",
             author: "M T Vasudevan Nair",
             image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTECvEnIrMhhWLnqeXsac1VTN8qqAttC4D9Jg&s"
         },
         {
-            id: 7,
+            Id: 7,
             title: "Manushyanu Oru Aamukham",
             author: "Subhash Chandran",
             image: "https://upload.wikimedia.org/wikipedia/en/5/5a/Manushyanu_Oru_Aamukham.jpg"
         },
         {
-            id: 8,
+            Id: 8,
             title: "Ente Katha",
             author: "Kamala Surayya",
             image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkxiZ3Ivi_ytD7nYLy4T19awTrH6s6S10rsA&s"
         },
         {
-            id: 9,
+            Id: 9,
             title: "Oru Sankeerthanam Pole",
             author: "Perumbadavam Sreedharan",
             image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnx9pq_5s9hWlXNOTVP-Sp6O1CWoNV3lnliw&s"
         },
         {
-            id: 10,
+            Id: 10,
             title: "Randidangazhi",
             author: "Thakazhi Sivasankara Pillai",
             image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1460745660l/12588437.jpg"
